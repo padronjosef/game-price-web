23eea0413362e8aecf5e0faf32565a7bfe7b9177
Jose Padron <josepadron.go@gmail.com>
2026-04-03 05:41:19 -0500

    [patch] Add Suspense boundary for useSearchParams in search page

--- src/app/(main)/search/page.tsx
+++ src/app/(main)/search/page.tsx
@@ -1,7 +1,13 @@
+import { Suspense } from "react";
 import { SearchTemplate } from "../../components/search/templates/SearchTemplate";
+import { SearchSkeleton } from "../../components/search/atoms/SearchSkeleton";
 
 const SearchPage = () => {
-  return <SearchTemplate />;
+  return (
+    <Suspense fallback={<SearchSkeleton />}>
+      <SearchTemplate />
+    </Suspense>
+  );
 };
 
 export default SearchPage;
