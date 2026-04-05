b08f205f9d801017b75ee2670a078d66a20fa2cd
Jose Padron <josepadron.go@gmail.com>
2026-04-05 00:55:37 -0500

    [patch] Wrap SearchTemplate in Suspense for useSearchParams

--- src/app/(main)/search/page.tsx
+++ src/app/(main)/search/page.tsx
@@ -1,5 +1,10 @@
+import { Suspense } from "react";
 import { SearchTemplate } from "@/app/components/search/templates/SearchTemplate";
 
-const SearchPage = () => <SearchTemplate />;
+const SearchPage = () => (
+  <Suspense>
+    <SearchTemplate />
+  </Suspense>
+);
 
 export default SearchPage;
